abee17de5c083b4a506fd0ae3e5b5ba964a06481
matib83 <efbingenieria@gmail.com>
2022-10-20 13:01:36 -0300

    5.11 Proptypes para 2 componentes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
   handLeLogin,
@@ -36,6 +37,14 @@ const LoginForm = ({
       </form>  
     </div>
   )
+}
+
+LoginForm.propTypes = {
+  handLeLogin: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  setUsername: PropTypes.func.isRequired,
+  password: PropTypes.string.isRequired,
+  setPassword: PropTypes.func.isRequired
 }
 
 export default LoginForm
